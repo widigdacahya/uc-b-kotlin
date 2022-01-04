35c309f4274b0e5983a25b25c1ece516667d35ff
widigdacahya <rizkiwidigda@gmail.com>
2022-01-05 01:58:07 +0700

    10_collections_intro

---
+++ 10_collection/src/0_intro_collections.kt
@@ -0,0 +1,30 @@
+fun main(args:Array<String>) {
+    println("It gonna have its own section :) ")
+
+
+    /*
+    * [COLLECTION]
+    *  > List
+    *   Ordered Collection
+    *   Element can be accessed by the position(index)
+    *   Can contain duplicate element
+    *   Example : A sentence is a list of words, they gave an order and can be repeated
+    *
+    *  > Set
+    *   Group of unique elements
+    *   The order has no significance
+    *   we could fo through the element, one by one(iterate over them but the order is not defined)
+    *   Example : A deck of card
+    *
+    *  > Map
+    *   a set of key-value pairs
+    *   Keys are unique(set)
+    *   Each key mapt to exactly one value
+    *   value can be duplicate
+    *   Example : a Dictionary where each Maps word to definitions
+    *
+    * ----Iterator, an object that loops through elements of a collection
+    *     this gonna be useful when applying the sam operation to all elements
+    * */
+
+}
