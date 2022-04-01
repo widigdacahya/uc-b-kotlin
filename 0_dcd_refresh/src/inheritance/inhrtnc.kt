769b8077f3a6758516142a3e31c93b537d2f6114
widigdacahya <rizkiwidigda@gmail.com>
2022-04-01 18:48:16 +0700

    Multiple Inheritance
    Using interface, other kin inheritance is Hieracical, its like if we have open class animal, open class carnivore(that extend Animal), then we have Cat extend Carnovre and Lion extend Carnivore. Other kind is Hybrid Inheritance, Class A as super, then Class C and D extends A, then we have class D extend C and B

--- 0_dcd_refresh/src/inheritance/inhrtnc.kt
+++ 0_dcd_refresh/src/inheritance/inhrtnc.kt
@@ -31,6 +31,48 @@ class JSHCat : Carnivore() {
 
 }
 
+
+//for [ multiple Inheritance ]
+interface Ovipar {
+    var egg:Int
+    fun layEgg()
+}
+
+interface Vivipar {
+    var bornSkill: String
+    fun giveMilkToCHild()
+}
+
+interface Ovovivipar: Ovipar,Vivipar
+
+class Snake(override var egg: Int, override var bornSkill: String) : Ovovivipar {
+    override fun layEgg() {
+        println("I am laying egg and left the egg, kind a $egg eggs")
+    }
+
+    override fun giveMilkToCHild() {
+        println("Not doing this I think")
+    }
+
+}
+
+
+class WhatANimal : Ovovivipar {
+    override var bornSkill: String = "Average"
+    override var egg: Int = 20
+
+    override fun layEgg() {
+        println("Lay egg about $egg in the garden of human")
+    }
+
+    override fun giveMilkToCHild() {
+        println("WHole day until the kid strong enough to fin eat by themself")
+    }
+}
+// multiple inheritance
+
+
+
 fun main(args: Array<String>) {
 
     //Single level inheritence
@@ -42,5 +84,24 @@ fun main(args: Array<String>) {
     val jsh = JSHCat()
     jsh.eat()
     jsh.walk()
+
+
+    println()
+    println()
+    //multiple INheritance
+    val mySnake = Snake(10, "none")
+    println("Bornskill : ${mySnake.bornSkill}")
+    println("Egg : ${mySnake.egg}")
+    mySnake.layEgg()
+    mySnake.giveMilkToCHild()
+
+    println()
+    val someAnimalWeDontknow = WhatANimal()
+    someAnimalWeDontknow.egg = 100
+    println("HOw manny egg : ${someAnimalWeDontknow.egg}")
+    someAnimalWeDontknow.layEgg()
+
+
+
 
 }
